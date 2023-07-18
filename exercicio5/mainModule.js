5b9466158dfa404d75f15247c87fc9c8eb6324d6
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-07-17 23:37:03 -0300

    [M1S06] EX5- Filtros

---
+++ exercicio5/mainModule.js
@@ -0,0 +1,19 @@
+// Crie dois módulos em arquivos distintos: filtrarPares.js e somaImpares.js.
+// No módulo filtrarPares.js, exporte uma função chamada filtrarPares, que recebe um array de números como parâmetro e retorna um novo array contendo apenas os números pares.
+// No módulo somaImpares.js, exporte uma função chamada somaImpares, que recebe um array de números como parâmetro e retorna a soma de todos os números ímpares.
+// No módulo principal, importe as duas funções e solicite ao usuário que insira um array de números.
+// Utilize a função filtrarPares para obter um novo array contendo apenas os números pares do array fornecido pelo usuário.
+// Em seguida, utilize a função somaImpares para calcular e exibir no console a soma dos números ímpares do array fornecido pelo usuário.
+
+const filtrarPares = require('./filtrarPares');
+const somaImpares = require('./somaImpares');
+
+const numeros = [
+  1, 2, 4, 9, 10, 23, 42, 54, 67, 85, 90, 103
+]
+
+const numerosPares = filtrarPares(numeros);
+console.log(numerosPares);
+
+const numerosImpares = somaImpares(numeros);
+console.log(numerosImpares);
